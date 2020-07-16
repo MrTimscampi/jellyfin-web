65f0ef6ff6114a6006e81c3cd5e685ddf6ab3780
MrTimscampi <julien.machiels@protonmail.com>
2020-07-16 22:17:36 +0200

    Prefer const over let when possible

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
         'no-multiple-empty-lines': ['error', { 'max': 1 }],
         'no-trailing-spaces': ['error'],
         'one-var': ['error', 'never'],
+        'prefer-const': ['error', {'destructuring': 'all'}],
         'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': false }],
         'semi': ['error'],
         'space-before-blocks': ['error'],
